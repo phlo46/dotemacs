5f9b799d0457e9da1ab4d8816d48f77023ee291b
longg <ngphilong46@gmail.com>
2018-06-21 11:57:40 +0700

    Add a new keybinding for `multiple-cursors`

--- init.el
+++ init.el
@@ -306,7 +306,8 @@
   :bind (("C-S-c C-S-c" . mc/edit-lines)
          ("C->" . mc/mark-next-like-this)
          ("C-<" . mc/mark-previous-like-this)
-         ("C-c C-<" . mc/mark-all-like-this)))
+         ("C-c C-<" . mc/mark-all-like-this)
+         ("C-c C-s" . mc/mark-all-symbols-like-this-in-defun)))
 
 ;;Emmet mode
 (use-package emmet-mode
