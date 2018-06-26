f4841fd462792957a4d4311aef9fe413c518c731
longg <ngphilong46@gmail.com>
2018-06-26 22:24:59 +0700

    Add bindings for `dumb-jump`

--- init.el
+++ init.el
@@ -254,8 +254,11 @@
 ;; dumb-jump
 (use-package dumb-jump
   :ensure t
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
   :config
-  (dumb-jump-mode)
   (setq dumb-jump-selector 'ivy))
 
 ;; ivy-mode
