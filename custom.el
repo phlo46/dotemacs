8539af57a63ff2437761372b076fa0dd104e8feb
longg <ngphilong46@gmail.com>
2018-07-11 17:53:30 +0700

    Update custom variables

--- custom.el
+++ custom.el
@@ -4,13 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cljr-hotload-dependencies t)
- '(company-ghc-show-info t)
- '(elpy-rpc-python-command "python3")
  '(es-always-pretty-print t)
  '(eshell-modules-list
    (quote
     (eshell-alias eshell-banner eshell-basic eshell-cmpl eshell-dirs eshell-glob eshell-hist eshell-ls eshell-pred eshell-prompt eshell-script eshell-term eshell-tramp eshell-unix)))
- '(flycheck-check-syntax-automatically (quote (save)))
  '(flycheck-python-flake8-executable "/usr/local/bin/python3")
  '(gnus-thread-sort-functions (quote (gnus-thread-sort-by-most-recent-date)))
  '(httpd-port 8100)
@@ -18,10 +15,9 @@
  '(org-agenda-files (quote ("~/Dropbox/org-mode/todo.org")))
  '(package-selected-packages
    (quote
-    (swift-mode dumb-jump eyebrowse rjsx-mode clj-refactor yasnippet-snippets intero haskell-mode indium diminish zenburn-theme magit nodejs-repl origami mode-icons ag sql-indent tern-auto-complete sly tide sly-mode es-mode highlight-chars projectile company-restclient restclient persp-mode ace-window restart-emacs use-package counsel racket-mode geiser yaml-mode web-mode utop undo-tree tuareg python-environment py-autopep8 paredit neotree multiple-cursors markdown-mode json-mode inflections go-mode flycheck-ocaml flycheck-elm exec-path-from-shell epc emmet-mode elpy company-tern auto-complete)))
+    (sly helpful expand-region goto-last-change buffer-move which-key company-quickhelp cmake-mode company-c-headers clang-format irony-eldoc flycheck-irony company-irony irony try flycheck js2-mode tern company avy ivy highlight-thing go-guru company-go dumb-jump eyebrowse rjsx-mode clj-refactor yasnippet-snippets intero haskell-mode indium diminish magit nodejs-repl origami mode-icons ag sql-indent tern-auto-complete tide sly-mode es-mode highlight-chars projectile company-restclient restclient persp-mode ace-window restart-emacs use-package counsel racket-mode geiser yaml-mode web-mode utop undo-tree tuareg python-environment py-autopep8 paredit neotree multiple-cursors markdown-mode json-mode inflections go-mode flycheck-ocaml flycheck-elm exec-path-from-shell epc emmet-mode elpy company-tern auto-complete)))
  '(persp-keymap-prefix "p")
  '(persp-nil-name "emacs")
- '(racket-program "racket")
  '(safe-local-variable-values
    (quote
     ((Base . 10)
@@ -29,7 +25,7 @@
      (Syntax . ANSI-Common-Lisp)
      (encoding . utf-8))))
  '(sly-net-coding-system (quote utf-8-unix))
- '(tramp-default-method "ssh"))
+ '(tramp-default-method "ssh" nil (tramp)))
 
 
 (custom-set-faces
