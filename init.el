e5814929742246f8bba04e254c1a0f64ab4c91c2
Long Nguyen <ngphilong46@gmail.com>
2020-04-06 20:32:06 +0700

    Remove dumb-jump custom config from init file
    These values should be persisted (if necessary) to custom.el instead
    of init.el.

--- init.el
+++ init.el
@@ -273,10 +273,7 @@
   :bind (("M-g o" . dumb-jump-go-other-window)
          ("M-g j" . dumb-jump-go)
          ("M-g x" . dumb-jump-go-prefer-external)
-         ("M-g z" . dumb-jump-go-prefer-external-other-window))
-  :config
-  (setq dumb-jump-selector 'ivy)
-  (setq dumb-jump-force-searcher 'ag))
+         ("M-g z" . dumb-jump-go-prefer-external-other-window)))
 
 ;; ivy-mode
 (use-package ivy
