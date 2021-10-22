391960c4b135555dcf73a81967408efd3eb3e9a7
Phi-Long Nguyen <ngphilong46@gmail.com>
2021-10-22 13:25:17 +0700

    dumb-jump: Bump to the latest version
    Remove rarely-used binding keys.
    
    In recent versions, these dumb-jump-go* are marked obsoleted but I
    still keep the 2 main go* keys to work with files whose mode has
    shadowed the M-. xref key such as sly-mode. Another reason is that
    completing-read pop-up feature is not ready for my current emacs 27.
    
    These remaining binding keys will be removed after emacs 28.1 comes out.

--- init.el
+++ init.el
@@ -277,9 +277,9 @@
 (use-package dumb-jump
   :straight t
   :bind (("M-g o" . dumb-jump-go-other-window)
-         ("M-g j" . dumb-jump-go)
-         ("M-g x" . dumb-jump-go-prefer-external)
-         ("M-g z" . dumb-jump-go-prefer-external-other-window)))
+         ("M-g j" . dumb-jump-go))
+  :config
+  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
 
 ;; ivy-mode
 (use-package ivy
