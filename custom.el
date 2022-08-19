b9e348ed2e397b00dda749736241ef06e3249041
Phi-Long Nguyen <ngphilong46@gmail.com>
2022-08-19 21:17:29 +0700

    browse-at-remote: Uses the commit hash to open a git remote link
    I prefer the commit hash over the default option (branch name) to make
    ref links to be more freeze, reproducible.

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(browse-at-remote-prefer-symbolic nil)
  '(cljr-hotload-dependencies t)
  '(company-show-quick-access t)
  '(es-always-pretty-print t)
