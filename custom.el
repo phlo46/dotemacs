569bd1413e0be4ca752e393cb83d4538f212ad6f
Phi-Long Nguyen <ngphilong46@gmail.com>
2024-01-18 21:02:17 +0700

    vterm: Remove custom face vterm-color-black
    Recently vterm differentiate foreground and background colors. The
    current custom face somehow makes k9s UI color running in vterm
    buffers very hard to look at. So I revert this custom face and go back
    to use default vterm faces which makes things normal again.

--- custom.el
+++ custom.el
@@ -51,5 +51,4 @@
  '(corfu-current ((t (:background "black" :foreground "white"))))
  '(corfu-default ((t (:background "gray31"))))
  '(embark-verbose-indicator-documentation ((t (:inherit completions-common-part))))
- '(marginalia-documentation ((t (:inherit completions-common-part))))
- '(vterm-color-black ((t (:background "dark gray" :foreground "black")))))
+ '(marginalia-documentation ((t (:inherit completions-common-part)))))
