2d112d8fb183baff74d80024821290dac34b537f
Phi-Long Nguyen <ngphilong46@gmail.com>
2026-09-12 21:39:31 +0700

    defuns: Remove obsolete sudo-edit helper
    Emacs 30 provides tramp-revert-buffer-with-sudo for reopening the
    current file or Dired directory with elevated permissions. Other
    local root files can be opened directly with TRAMP's concise /sudo::
    path syntax.

--- defuns.el
+++ defuns.el
@@ -15,19 +15,6 @@
 
 (global-set-key (kbd "C-x |") 'toggle-frame-split)
 
-;; sudo edit
-(defun sudo-edit (&optional arg)
-  "Edit currently visited file as root.
-   With a prefix ARG prompt for a file to visit.
-   Will also prompt for a file to visit if current
-   buffer is not visiting a file."
-
-  (interactive "P")
-  (if (or arg (not buffer-file-name))
-      (find-file (concat "/sudo:root@localhost:"
-                         (ido-read-file-name "Find file(as root): ")))
-    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
-
 (defun occur-non-ascii ()
   "Find any non-ascii characters in the current buffer."
   (interactive)
